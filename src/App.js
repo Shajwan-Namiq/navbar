a1ab7189bcbaefd68e2360cecd586819cf914561
Shajwan <shajwannamiqsalh@gmail.com>
2022-10-18 16:14:38 +0300

    navbar : using useRef

---
+++ src/App.js
@@ -0,0 +1,15 @@
+ 
+import Navbar from "./Navbar";
+import "./App.css";
+ 
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <p className="flex justify-center p-32">useing UseRef </p>
+    </>
+  );
+}
+
+export default App;
